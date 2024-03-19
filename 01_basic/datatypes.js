35ad8e8e037b95631bd07e6167387ed5889869c7
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-03-19 12:38:54 +0530

    variabels and datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,28 @@
+"use strict";  // treat all js code as newer version
+
+// alert( 3 + 3) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // make code readable
+
+    console.log("Mitu")
+
+    let name = "Butu"
+    let age = 12
+    let isLoggedIn = true
+
+    //DATA Types :
+    //number : 1,2,3... /2 to power 24
+    //bigint 
+    //string : "dama"
+    //bolean : true/false
+    //null : standalone value ,object
+    //undefined : undefined
+    //symbol : unique
+
+
+    //object
+
+    console.log(typeof null); // object
+    console.log(typeof undefined); // undefined
---
+++ 01_basic/variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345;
+let accountEmail = "qwery@er.com"
+var accountPassword = "12345"
+accountCity = "cuttack"
+
+let accountState;
+
+// accountId = 7 //not allowed
+
+accountEmail = "harz@rt.com"
+accountpassword = "24563"
+accountCity = "Bhubaneswar"
+
+// prefer not to use var
+// because of issue in block and scope {} and functional scope 
+
+console.table([accountId,accountEmail,accountPassword,accountCity ,accountState])
+
