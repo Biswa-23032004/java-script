87efbf9bc08171e4de7c68bdaa8f348514b75e6e
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-04-01 21:07:55 +0530

    String

--- 01_basic/string.js
+++ 01_basic/string.js
@@ -1,4 +1,27 @@
 const name ="Bunty"
 const repoCount = 10
 
-console.log('My name is ${name} and my repocount is ${repoCount}');
+console.log(`My name is ${name} and my repocount is ${repoCount}`);
+
+const fileName = new String("biswa.js")
+
+// console.log(fileName[1]);
+// console.log(fileName.__proto__);
+
+// console.log(fileName.charAt(3));
+// console.log(fileName.indexOf('b'));
+
+
+const newString = fileName.substring(0, 4)
+console.log(newString);
+
+const newString1 = fileName.slice(1, 4)
+console.log(newString1);
+
+const newString2 = "   Shyam   "
+// console.log(newString2);
+// console.log(newString2.trim());
+
+// Deleting extra space.
+
+
