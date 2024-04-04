539d6cb227b63572d14df168aa3195e508f75ae3
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-04-04 08:11:07 +0530

    date&time method

---
+++ 01_basic/dateandtime.js
@@ -0,0 +1,29 @@
+let xDate = new Date()
+// add more methods
+
+// console.log(xDate.toString());
+// console.log(xDate.toDateString());
+// console.log(xDate.toLocaleString());
+// console.log(typeof xDate);
+
+let creatDate = new Date("04-04-2024")
+
+// console.log(creatDate.toLocaleString());
+
+let todayTime = Date.now()
+// console.log(todayTime);
+// console.log(creatDate.getTime());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);  
+// +1 for month start from 0
+console.log(newDate.getDay());
+
+// '${newDate.getDay()}' day and the time
+
+
+
+
+
+
