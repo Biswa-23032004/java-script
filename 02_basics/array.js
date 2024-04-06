6193fe902223ed6f70cfaaf5b6a2c82688372fad
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-04-06 20:01:58 +0530

    array1 & 2

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -24,17 +24,43 @@ const newArr6 = newArr.shift()
 // console.log(newArr.includes(4));
 // console.log(newArr.indexOf(2));
 
-console.log("Hari",newArr);
+// console.log("Hari",newArr);
 
 const myArr = newArr.slice(1,5)
 
-console.log(myArr);
-console.log("C",newArr);
+// console.log(myArr);
+// console.log("C",newArr);
 
 const myArr2 = newArr.splice(1,5)
 //splice change the main array
-console.log(myArr2);
-console.log("C",newArr);
+// console.log(myArr2);
+// console.log("C",newArr);
 
 
+/* More array concepts*/
 
+
+const my_heros = ["Ganesh ji","Hanuman ji","Prhlad Maharaj"]
+const new_heros = ["Hari","Narayan"]
+
+const all_heros = [...my_heros, ...new_heros]
+// console.log(all_heros);
+
+const allheros = my_heros.concat(new_heros)
+// console.log(allheros);
+
+
+const all2_heros = my_heros.push(new_heros)
+// console.log(all2_heros);
+// console.log(my_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+let num1 = 100
+let num2 = 200
+let num3 = 300
+
+console.log(Array.of(100,200,300));
