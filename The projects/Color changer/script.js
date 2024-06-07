1b89bb2ee90b82f38b931933e2fbfecfe6a3a5a0
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-06-07 13:56:41 +0530

    color switcher

--- The projects/Color changer/script.js
+++ The projects/Color changer/script.js
@@ -18,5 +18,31 @@ button.forEach(function (button) {
         if (event.target.id === 'yellow') {
             body.style.backgroundColor = event.target.id;
         }
+
+        // when we use switsh case for loop.
+
+        // switch (event.target.id){
+
+        //     case "grey":
+        //     body.style.backgroundColor = event.target.id;
+      
+        //     // We could have written the thebody.style.backgroundColor = grey;
+        //     // But writting in the above way makes the code more efficient and it is a good practice
+        //     break;
+      
+        //     case "purple":
+        //     body.style.backgroundColor = event.target.id;
+        //     break;
+      
+        //     case "blue":
+        //     body.style.backgroundColor = event.target.id;
+        //     break;
+      
+        //     case "yellow":
+        //     body.style.backgroundColor = event.target.id;
+        //     break;
+      
+        //     default:
+        //     break;
     })
 })
