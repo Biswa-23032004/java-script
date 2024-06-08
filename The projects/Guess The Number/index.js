57c531a20c64fdf863e9ca1dc9ee959264e04fb7
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-06-08 23:00:03 +0530

    Number guess game

---
+++ The projects/Guess The Number/index.js
@@ -0,0 +1,10 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guesses = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+
