36e5c0be92ba47dfc4aa6a3233d5b74aa5b8a034
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-06-24 21:38:36 +0530

    Static property

---
+++ 10_advance_two/static_prop.js
@@ -0,0 +1,32 @@
+// Static class methods are defined on the class itself.
+
+// We cannot call a static method on an object, only on an object class.
+
+class User {
+    constructor(name) {
+        this.username = name;
+    }
+
+    password(){
+        return "1234"
+    }
+
+     static hello() {
+        console.log('Hello ${this.username}');
+    }
+}
+
+const hars = new User("Hars")
+//hars.hello() //hars.hello is not a function 
+
+class Monitor extends User{
+    constructor(name,Email){
+        super(name)
+        this.email = Email
+    }
+}
+
+const Class = new Monitor("Imran","imran@gmail.com")
+
+console.log(Class.password()); //1234
+Class.hello() //Class.hello is not a function 
