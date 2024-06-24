15c9105037404024ee32527a04e29b237b002a88
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-06-24 19:54:22 +0530

    classes in js

--- 10_advance_two/myClasses.js
+++ 10_advance_two/myClasses.js
@@ -1,7 +1,42 @@
 // After ES6
 
+// Use the keyword class to create a class.
+
+// Always add a method named constructor():
+
+// class ClassName {
+//     constructor() { ... }
+//   }
+
+// Example
+// class Car {
+//   constructor(name, year) {
+//     this.name = name;
+//     this.year = year;
+//   }
+// }
+// The example above creates a class named "Car".
+
+// The class has two initial properties: "name" and "year".
+
+// When you have a class, you can use the class to create objects:
+
+// Example:
+// const myCar1 = new Car("Ford", 2014);
+// const myCar2 = new Car("Audi", 2019);
+
+// The example above uses the Car class to create two Car objects.
+
+// The constructor method is a special method:
+
+// It has to have the exact name "constructor"
+// It is executed automatically when a new object is created
+// It is used to initialize object properties
+// If you do not define a constructor method, JavaScript will add an empty constructor method.
+
+
 class User {
-        // when a object initialize from class in thet time automatically constructor will be called.
+    // The constructor method is called automatically when a new object is created.
     constructor(userName, email, password){
         this.userName = userName;
         this.email = email;
